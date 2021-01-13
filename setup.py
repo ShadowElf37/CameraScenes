1cf52e45b5e2d0edb8d0c93ec3fa2a30ebc8d681
ShadowElf37 <ykey-cohen@emeryweiner.org>
2021-01-13 00:11:43 -0600

    codesign

--- setup.py
+++ setup.py
@@ -16,9 +16,13 @@ if system() == 'Windows':
 elif system() == 'Darwin':
     pyi.run([
         'client.py',
-        '--onefile',
+        '--onedir',
+        '--osx-bundle-identifier=com.stuff.Proscenium',
         '--windowed',
         '--add-data=images/favicon.png:images',
         '--icon=images/favicon.icns',
         '--name=Proscenium'
     ])
+
+    import subprocess
+    subprocess.run('codesign --force --deep --sign "Developer ID Application: Karen Coveler (K559Z5J335)" dist/Proscenium.app')
